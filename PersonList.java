461a13d7079daa633a70d5fa22d63e5d86b639ea
mohammed92-max <shahrukhkhandb@gmail.com>
2020-05-24 10:43:11 +0530

    added PersonList.java file

---
+++ PersonList.java
@@ -0,0 +1,16 @@
+package assignment;
+import java.util.*;
+
+public class PersonList 
+{
+	private List<Person> persons;
+
+	public List<Person> getPersons() 
+	{
+		return persons;
+	}
+	public void setPersons(List<Person> persons) 
+	{
+		this.persons = persons;
+	}	
+}
